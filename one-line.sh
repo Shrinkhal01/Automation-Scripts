9ed5e12c9ce5e91a05aa29643c9534ae8c59a5c9
Shrinkhal <97280075+Shrinkhal01@users.noreply.github.com>
2025-02-17 14:43:44 +0530

    simple file
    inputo of two number and print which is greater

--- one-line.sh
+++ one-line.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
-read -r "Enter a number: " num0
+read -p "Enter a number: " num0
 
-read -r "Enter another number to compare : " num1
+read -p "Enter another number to compare: " num1
 
-if[[$num0 -gt $num1]]
+if [[ $num0 -gt $num1 ]]
 then
-    echo "The first number is greater than the second number."
+    echo "The first number $num0 is greater than the second number $num1."
 else
-    echo "The first number is less than the second number."
+    echo "The first number $num0 is less than or equal to the second number $num1."
 fi
